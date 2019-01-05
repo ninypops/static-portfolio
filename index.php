137335cd1cba45ad465e5cb77ddb25471c37f855
228C39 <ninypopsgame@gmail.com>
2019-01-05 15:30:41 +0000

    index.php and css commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+
+<section class="left">
+  <h1>Nina Limbrick</h1>
+  <p>Lorem ipsum dolor sit amet, graeci lucilius delicatissimi ut mel. His ex minim theophrastus. Suas voluptaria ne cum, te nec fugit praesent suavitate. Viris voluptatum consectetuer eam id. Viris equidem consetetur est ei.</p>
+
+<p>Facete oporteat dissentiunt usu ad. Sumo utroque accumsan has id, usu appellantur liberavisse ad, ne omnis veniam eam. Nec vero pertinax definiebas cu. Qui no error vulputate, dolorum accusam nec ei, no laudem tibique tacimates ius. Nec ne liber quaeque, quis tation assueverit at quo.</p>
+</section>
+  
+<section class="right">
+</section>
+
+<footer>
+    <form >
+        <input type="text" placeholder="name" name="name">
+        <input type="email" placeholder="email" name="email">
+        <select name="enquiry">
+            <option value="contract work">contract work</option>
+            <option value="freelance work">freelance work</option>
+            <option value="hiring">hiring</option>
+            <option value=""></option>
+        </select>
+        <input type="submit" value="Submit">
+    </form>
+</footer>
+
+</body>
+</html>
